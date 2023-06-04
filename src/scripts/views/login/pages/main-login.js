2dc972f4ed62f94a49e24b0373be0491da49303f
Rizki <rizkidliyaulhaq22@gmail.com>
2023-06-04 20:56:28 +0700

    Mengubah Halaman Login

--- src/scripts/views/login/pages/main-login.js
+++ src/scripts/views/login/pages/main-login.js
@@ -4,20 +4,20 @@ import { async } from "regenerator-runtime";
 
 const mainLogin = {
     async render() {
-        return `<section class="vh-100" style="background-color: #eee;">
+        return `<section class="vh-100" style="background-color:#1f2122;">
         <div class="container h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-lg-12 col-xl-11">
-              <div class="card text-black" style="border-radius: 25px;">
-                <div class="card-body p-md-5">
+            <div class="col-lg-12 col-xl-11" style="background-color#850f37; border-radius: 23px";>
+              
+                <div class="card-body p-md-5" style="background-color:#e75e8d; border-radius: 23px">
                   <div class="row justify-content-center">
                     <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
       
-                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login Member</p>
+                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style="color:#850f37;" >Login Member</p>
       
                       <form class="mx-1 mx-md-4" id="loginForm">
       
-                        <div class="d-flex flex-row align-items-center mb-4">
+                        <div class="d-flex flex-row align-items-center mb-4" >
                           <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                           <div class="form-outline flex-fill mb-0">
                             <input type="email" id="form3Example3c" class="form-control" placeholder="Your Email" required />
@@ -39,7 +39,7 @@ const mainLogin = {
 
       
                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="submit" class="btn btn-primary btn-lg">Login</button>
+                          <button type="submit" class="btn btn-primary btn-lg" style="background-color:#850f37; border-color:#e75e8d">Login</button>
                         </div>
       
                       </form>
@@ -53,7 +53,7 @@ const mainLogin = {
                     </div>
                   </div>
                 </div>
-              </div>
+      
             </div>
           </div>
         </div>
