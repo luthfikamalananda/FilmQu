a19ee544d55436d8aea5825ed51cf850d65aff16
Rizki <rizkidliyaulhaq22@gmail.com>
2023-06-04 23:22:46 +0700

    Meubah halaman login

--- src/scripts/views/login/pages/main-login.js
+++ src/scripts/views/login/pages/main-login.js
@@ -4,16 +4,16 @@ import { async } from "regenerator-runtime";
 
 const mainLogin = {
     async render() {
-        return `<section class="vh-100" style="background-color:#1f2122;">
+        return `<section class="vh-100" style="background-image:url('../assets/images/bglogin.jpg');">
         <div class="container h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-lg-12 col-xl-11" style="background-color#e75e8d96; border-radius: 23px";>
-              
-                <div class="card-body p-md-5" style="background-color:#e75e8d96; border-radius: 23px">
+            <div class="col-lg-12 col-xl-11" border-radius: 23px";>
+            <div class="card text-black" style="background-color:#1f2122; border-radius: 23px;">
+                <div class="card-body p-md-5" style="background-color:#1f2122; border-radius: 23px;">
                   <div class="row justify-content-center">
-                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1" style="background-color:#27292a; border-radius: 23px;" >
       
-                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style="color:white; font-family:'Poppins', sans-serif;" >Login Member</p>
+                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style="color:#ec6090; font-family:'Poppins', sans-serif;" >LOGIN</p>
       
                       <form class="mx-1 mx-md-4" id="loginForm">
       
@@ -39,19 +39,18 @@ const mainLogin = {
 
       
                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="submit" class="btn btn-primary btn-lg" style="background-color:black; border-color:#e75e8d96; font-family:'Poppins', sans-serif;"">Login</button>
+                          <button type="submit" class="btn btn-primary btn-lg" style="background-color:#ec6090; border-color:#e75e8d96; font-family:'Poppins', sans-serif;"">Login</button>
                         </div>
       
                       </form>
       
                     </div>
                     <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                      <img src=../assets/images/Filmqu.png
-                        class="img-fluid" alt="Sample image">
+                      <img src=../assets/images/Filmqu.png class="img-fluid" alt="Sample image">
                     </div>
                   </div>
                 </div>
-      
+                </div>
             </div>
           </div>
         </div>
