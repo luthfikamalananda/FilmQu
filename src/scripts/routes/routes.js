be8dab653d696c2cd6aa9c5dc6a601c39d1f8f24
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-04 19:05:27 +0700

    add routing and app shell on the main login

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,41 @@
+// import dashboardHome from "../../views/dashboard/pages/dashboard";
+// import accountsAdmin from "../../views/home/pages/akun";
+// import dashboardAdmin from "../../views/home/pages/dashboard";
+// import dataAdmin from "../../views/home/pages/data";
+// import editData from "../../views/home/pages/editData";
+// import editMenu from "../../views/home/pages/editMenu";
+// import menuAdmin from "../../views/home/pages/menu";
+// import profil from "../../views/home/pages/profil";
+// import mainLogin from "../../views/login/pages/main-login";
+import mainLogin from "../views/login/pages/main-login";
+// import mainRegister from "../../views/register/pages/main-register";
+
+
+
+// Register routes
+// const registerRoutes = {
+//   '/': mainRegister,
+// };
+
+// Login routes
+const loginRoutes ={
+  '/': mainLogin,
+};
+
+// const adminRoutes = {
+//   '/': dashboardAdmin,
+//   '/akun': accountsAdmin,
+//   '/data': dataAdmin,
+//   '/menu': menuAdmin,
+//   '/profil': profil,
+//   '/editdata/:id': editData,
+//   '/editmenu/:id': editMenu
+// }
+
+// const HomeRoutes = {
+//   '/': dashboardHome,
+// }
+
+export {
+   loginRoutes
+};
