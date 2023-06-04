c9a4f5011bb4dfd18619f2c0b3217fe28e2efc86
Rizki <rizkidliyaulhaq22@gmail.com>
2023-06-04 23:36:32 +0700

    halaman login

--- src/scripts/views/register/pages/main-register.js
+++ src/scripts/views/register/pages/main-register.js
@@ -3,16 +3,16 @@ import { async } from "regenerator-runtime";
 
 const mainRegister = {
     async render() {
-        return ` <section class="vh-100" style="background-color: #eee;">
+        return `<section class="vh-100" style="background-image:url('../assets/images/bglogin.jpg');">
         <div class="container h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-lg-12 col-xl-11">
-              <div class="card text-black" style="border-radius: 25px;">
-                <div class="card-body p-md-5">
+            <div class="col-lg-12 col-xl-11" border-radius: 23px";>
+            <div class="card text-black" style="background-color:#1f2122; border-radius: 23px;">
+                <div class="card-body p-md-5" style="background-color:#1f2122; border-radius: 23px;">
                   <div class="row justify-content-center">
-                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1" style="background-color:#27292a; border-radius: 23px;" >
       
-                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Daftar Admin</p>
+                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style="color:#ec6090; font-family:'Poppins', sans-serif;" >REGISTER</p>
       
                       <form class="mx-1 mx-md-4" id="registerForm">
       
@@ -44,28 +44,25 @@ const mainRegister = {
                           </div>
                         </div>
 
-                        <div class="form-check d-flex justify-content-center mb-4">
+                        <div class="form-check d-flex justify-content-center mb-4" style="color:white;">
                         <label class="form-check-label" for="form2Example3">
                         Sudah memiliki akun? silahkan <a href="../login">Login Disini</a>
                         </label>
                         </div>
       
                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="submit" class="btn btn-primary btn-lg">Register</button>
+                          <button type="submit" class="btn btn-primary btn-lg" style="background-color:#ec6090; border-color:#e75e8d96; font-family:'Poppins', sans-serif;"">Register</button>
                         </div>
       
                       </form>
       
                     </div>
                     <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-      
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        class="img-fluid" alt="Sample image">
-      
+                      <img src=../assets/images/Filmqu.png class="img-fluid" alt="Sample image">
                     </div>
                   </div>
                 </div>
-              </div>
+                </div>
             </div>
           </div>
         </div>
