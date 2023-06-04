f1a394f6717afa1a6809bde6f3e8c731f0c56d09
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-04 20:55:46 +0700

    Add Favourite page and change navbar anchor

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -7,6 +7,7 @@
 // import menuAdmin from "../../views/home/pages/menu";
 // import profil from "../../views/home/pages/profil";
 // import mainLogin from "../../views/login/pages/main-login";
+import favourite from "../views/home/pages/favourite";
 import homePage from "../views/home/pages/homepage";
 import profile from "../views/home/pages/profile";
 import mainLogin from "../views/login/pages/main-login";
@@ -27,6 +28,7 @@ const loginRoutes ={
 const homeRoutes = {
   '/': homePage,
   '/profile': profile,
+  '/favourite': favourite,
   '/editdata/:id': 'dsd',
 }
 
