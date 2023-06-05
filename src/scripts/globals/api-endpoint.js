ee8b08bd54637b62f5b7bdf406093ed3e084e4ce
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-05 16:51:53 +0700

    add endpoint for searching movies

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,6 +3,7 @@ import tmdbConfig from './tmdbConfig';
 const API_ENDPOINT = {
   NOW_PLAYING: `${tmdbConfig.BASE_URL}movie/now_playing?api_key=${tmdbConfig.KEY}&language=${tmdbConfig.DEFAULT_LANGUAGE}&page=1`,
   POPULAR: `${tmdbConfig.BASE_URL}movie/top_rated?api_key=${tmdbConfig.KEY}&language=${tmdbConfig.DEFAULT_LANGUAGE}&page=1`,
+  SEARCH: (query) => `${tmdbConfig.BASE_URL}search/movie?query=${query}&api_key=${tmdbConfig.KEY}&include_adult=false&language=${tmdbConfig.DEFAULT_LANGUAGE}&page=1`,
   DETAIL: (id) => `${tmdbConfig.BASE_URL}movie/${id}?api_key=${tmdbConfig.KEY}`,
 };
 
