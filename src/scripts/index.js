6ab4a4891ddee3769ef695bbaaa0f0956d1dc57b
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-08 16:59:44 +0700

    add member's name in navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,14 +4,11 @@ import AppHome from './views/home/app';
 
 
 
-// if(localStorage.getItem('user')) {
-//   window.location.href='/'
-//   const userAdmin = localStorage.getItem('user');
-//   const data = JSON.parse(userAdmin);
-//   document.getElementById('username').innerText = data.nama;
-//   document.getElementById('namauser').innerText = data.nama;
-//   document.getElementById('user').innerText = data.user;
-// }
+if(localStorage.getItem('user')) {
+  const user = localStorage.getItem('user');
+  const data = JSON.parse(user)
+  document.getElementById('profileNav').innerHTML = ` ${data.nama.split(' ')[0]} <img src="assets/images/profile-header.jpg" alt="">`;
+}
 
 const appHome = new AppHome({
   maincontent: document.querySelector('#mainhome'),
