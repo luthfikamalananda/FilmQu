16729f5fb87800ac1a92634f27c992d15cfd4941
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-08 17:33:13 +0700

    add different navabar when login or not

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,6 +8,13 @@ if(localStorage.getItem('user')) {
   const user = localStorage.getItem('user');
   const data = JSON.parse(user)
   document.getElementById('profileNav').innerHTML = ` ${data.nama.split(' ')[0]} <img src="assets/images/profile-header.jpg" alt="">`;
+} else {
+  const navFavourite = document.getElementById('favouriteNavLI');
+  const navReview = document.getElementById('reviewedNavLI');
+  const navProfile = document.getElementById('profileNav');
+  navFavourite.setAttribute('style', 'display: none;')
+  navReview.setAttribute('style', 'display: none;')
+  navProfile.setAttribute('href', '/login')
 }
 
 const appHome = new AppHome({
