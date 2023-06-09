31272d72f00db024c6bee71ffc5f2cd4ac21558a
Rizki <rizkidliyaulhaq22@gmail.com>
2023-06-10 00:12:24 +0700

    add delete button in review

--- src/scripts/views/home/pages/detail.js
+++ src/scripts/views/home/pages/detail.js
@@ -418,11 +418,11 @@ const detailPage = {
       <div class="comment mt-4 text-justify float-left">
         <hr style="color:white;">
         <div>
-        <p style="color:#ec6090;"><i class="fa fa-star" style="color:pink; font-size:14px;"></i> ${data.data().rating} </p>
-          <h5>${data.data().member_nama}</h5>
+          <p style="color:#ec6090;"><i class="fa fa-star" style="color:pink; font-size:14px;"></i> ${data.data().rating}</p>
+          <h5>${data.data().member_nama} <i class="fas fa-trash-alt" style="color:pink; font-size:15px; float:right;"><button></button></i></h5>
           <span style="color:grey; text-align:right;"> ${data.data().date}</span>
           <br>
-          <p style="color: white;padding-left:50px;">${data.data().content}</p>
+          <p style="color: white;padding-left:15px;">${data.data().content}</p>
         </div>
       </div>
       <br>
