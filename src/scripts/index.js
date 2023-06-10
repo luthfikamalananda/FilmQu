b9564c52ff6f186c8bbec2a7b2993a9c79577852
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-10 22:57:02 +0700

    change navbar for moderator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,13 +3,17 @@ import AppHome from './views/home/app';
 
 
 // Authentication
+const navFavourite = document.getElementById('favouriteNavLI');
+const navReview = document.getElementById('reviewedNavLI');
 if(localStorage.getItem('user')) {
   const user = localStorage.getItem('user');
   const data = JSON.parse(user)
+  if (data.status == 'moderator') {
+    navFavourite.setAttribute('style', 'display: none;')
+    navReview.setAttribute('style', 'display: none;')
+  }
   document.getElementById('profileNav').innerHTML = ` ${data.nama.split(' ')[0]} <img src="assets/images/profile-header.jpg" alt="">`;
 } else {
-  const navFavourite = document.getElementById('favouriteNavLI');
-  const navReview = document.getElementById('reviewedNavLI');
   const navProfile = document.getElementById('profileNav');
   navFavourite.setAttribute('style', 'display: none;')
   navReview.setAttribute('style', 'display: none;')
