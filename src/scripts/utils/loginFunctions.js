1c43fc03925779b171f688e0eff53b4dc562123b
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-10 22:54:09 +0700

    bugfix login admin

--- src/scripts/utils/loginFunctions.js
+++ src/scripts/utils/loginFunctions.js
@@ -21,74 +21,76 @@ const loginFunctions = {
                 mod = doc.data();
                 mod.id = doc.id;
             })
+            if (mod.password == data.password) { // Login Moderator
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Berhasil',
+                    text: 'Selamat login anda berhasil',
+                    showCloseButton: true,
+                    allowOutsideClick: false
+                })
+                localStorage.setItem('user', JSON.stringify(mod))
+                setTimeout(() => {
+                    window.location.href = './'
+                }, 2000);
+            } else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Gagal',
+                    text: 'Pastikan data yang anda masukkan benar',
+                    showCloseButton: true,
+                    allowOutsideClick: false
+                    })
+            }
         }
         if (querySnapshot.size > 0) {
             querySnapshot.forEach((doc) => {
                 user = doc.data();
                 user.id = doc.id;
             })
-        }
-        if (user.password == data.password) { // Login Member
-            Swal.fire({
-                icon: 'success',
-                title: 'Login Berhasil',
-                text: 'Selamat login anda berhasil',
-                showCloseButton: true,
-                })
-            const dataToDB = {
-                id: user.id,
-                status: 'active',
-                last_login: new Date().toISOString().split('T')[0],
-                email: user.email,
-                nama: user.nama,
-                password: user.password,
-                date_created: user.date_created,
-                film_favorit: user.film_favorit,
-                film_review: user.film_review,
-                foto_profil: user.foto_profil,
-                bio: user.bio
-            }
-            await setDoc(doc(db, "member", user.id), dataToDB);
 
-            localStorage.setItem('user', JSON.stringify(dataToDB))
-            setTimeout(() => {
-                window.location.href = '#/'
-            }, 2000);
-        } else{
-            Swal.fire({
-                icon: 'error',
-                title: 'Login Gagal',
-                text: 'Pastikan data yang anda masukkan benar',
-                showCloseButton: true,
-                }).then((result) => {
-                    /* Read more about isConfirmed, isDenied below */
-                    if (result.isConfirmed) {
-                      window.location.href = './';
-                      location.reload();
-                    } 
-                  })
+            if (user.password == data.password) { // Login Member
+                const dataToDB = {
+                    id: user.id,
+                    status: 'active',
+                    last_login: new Date().toISOString().split('T')[0],
+                    email: user.email,
+                    nama: user.nama,
+                    password: user.password,
+                    date_created: user.date_created,
+                    film_favorit: user.film_favorit,
+                    film_review: user.film_review,
+                    foto_profil: user.foto_profil,
+                    bio: user.bio
+                }
+                await setDoc(doc(db, "member", user.id), dataToDB);
+    
+                localStorage.setItem('user', JSON.stringify(dataToDB))
+    
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Berhasil',
+                    text: 'Selamat login anda berhasil',
+                    showCloseButton: true,
+                    }).then((result) => {
+                        /* Read more about isConfirmed, isDenied below */
+                        if (result.isConfirmed) {
+                          window.location.href = './';
+                          location.reload();
+                        } 
+                      })
+            } else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Gagal',
+                    text: 'Pastikan data yang anda masukkan benar',
+                    showCloseButton: true,
+                    allowOutsideClick: false
+                    })
+            }
         }
-        if (mod.password == data.password) { // Login Moderator
-            Swal.fire({
-                icon: 'success',
-                title: 'Login Berhasil',
-                text: 'Selamat login anda berhasil',
-                showCloseButton: true,
-                allowOutsideClick: false
-            })
-            localStorage.setItem('user', JSON.stringify(mod))
-            setTimeout(() => {
-                window.location.href = './'
-            }, 2000);
-        } else{
-            Swal.fire({
-                icon: 'error',
-                title: 'Login Gagal',
-                text: 'Pastikan data yang anda masukkan benar',
-                showCloseButton: true,
-                allowOutsideClick: false
-                })
-        }
+        
+        
     }
 }
 
