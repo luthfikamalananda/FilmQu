67347b45b47f112fec110da222ba7dbbc2455510
Rizki <rizkidliyaulhaq22@gmail.com>
2023-06-11 22:25:28 +0700

    responsive search page

--- src/scripts/views/home/pages/search.js
+++ src/scripts/views/home/pages/search.js
@@ -5,13 +5,13 @@ import UrlParser from "../../../routes/url-parser";
 
 const searchPage = {
     async render() {
-        return `<div class="container-fluid" >
+        return `<div class="container-fluid responsive-container-fluid" >
         <div class="row">
             <div class="col-lg-12">
-                <div class="page-content">
+                <div class="page-content responsive-page-content" style="padding:20px;">
 
                     <!-- ***** Most Popular Start ***** -->
-                    <div class="most-popular">
+                    <div class="most-popular responsive-most-popular">
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="heading-section">
@@ -39,11 +39,11 @@ const searchPage = {
         
         searchedMovies.forEach(movie => {
             searchMoviesContainer.innerHTML += `
-            <div class="col-lg-3 col-sm-6">
-                <div class="item">
+            <div class="col-lg-3 col-sm-6 col-6">
+                <div class="item" style="max-width:auto; padding:13px;">
                     <a href='#/detail/${movie.id}'><img src="${movie.backdrop_path ? tmdbConfig.BASE_IMAGE_URL + movie.backdrop_path : 'https://picsum.photos/id/666/800/450?grayscale'}" alt=""></a>
-                    <a href='#/detail/${movie.id}'><h4 class='titleName'>${movie.title}<br><span>${movie.release_date}</span></h4></a>
-                    <ul>
+                    <a href='#/detail/${movie.id}'><h4 class='responsive-h4-rev-like' style="max-width:auto;">${movie.title}<br><span>${movie.release_date}</span></h4></a>
+                    <ul class="star-rating">
                         <li><i class="fa fa-star"></i> ${movie.vote_average}</li>
                         <li><i class="fa fa-star"></i></li>
                     </ul>
