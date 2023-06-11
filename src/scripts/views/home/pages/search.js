faa003db917e84b77df8bad40b8fcc65b40b1fb7
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-06-12 01:39:30 +0700

    responsive search bar in the search page

--- src/scripts/views/home/pages/search.js
+++ src/scripts/views/home/pages/search.js
@@ -11,13 +11,13 @@ const searchPage = {
                 <div class="page-content responsive-page-content" style="padding:20px;">
 
                     <!-- ***** Most Popular Start ***** -->
-                    <div class="most-popular responsive-most-popular" style="margin-top:0px;background-color:transparent;">
+                    <div id="search-bar-page" class="most-popular responsive-most-popular" style="margin-top:0px;background-color:transparent;display:block;">
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="heading-section">
                                     <h4 style='text-align:center;'>Cari Film</h4>
                                 </div>
-                                <form id="search" action="#">
+                                <form action="#">
                                         <input style='background-color:#1f2122;color:white;width:100%;height: 50px;border: 0;border-radius: 40px;padding: 10px 70px 10px 32px;' type="text" placeholder="Film yang ingin anda cari.." id='searchTextInSearch' name="searchKeyword" />
                                         <span class="fa fa-search" style="float: right;margin-right: 16px;margin-top: -32px;position: relative;z-index: 2;color:white;"></span>
                                 </form>
@@ -27,7 +27,7 @@ const searchPage = {
                     <!-- ***** Most Popular End ***** -->
 
                     <!-- ***** Most Popular Start ***** -->
-                    <div class="most-popular responsive-most-popular">
+                    <div class="most-popular responsive-most-popular" style="margin-top:0px;">
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="heading-section">
